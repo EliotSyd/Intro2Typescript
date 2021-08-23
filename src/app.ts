4db822a44356466caee2f2fade5ea8dd56c0b9e3
EliotSyd <eliot.sydenham@gmail.com>
2021-08-23 12:54:18 +0000

    Add

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
-console.log("Hello, World");
+import { personGreeter } from "./personGreeter";
+
+const firstName = "Eliot";
+const secondName = "Sydenham";
+
+const fullName = '${firstName} ${secondName}';
+
+console.log('Hello ${fullName}');
+
+type Person = {
+    firstName: string;
+    secondName: string;
+    dateOfBirth?: Date;
+}
+
+const person : Person = {
+    firstName: 'Eliot',
+    secondName: 'Sydenham',
+    dateOfBirth: new Date (2000, 0, 1)
+};
+
+const sayHelloToPerson = (p: Person) => {
+    console.log(`Hello ${p.firstName} ${p.secondName}`)
+
+};
+sayHelloToPerson(person);
+
+
 
 
